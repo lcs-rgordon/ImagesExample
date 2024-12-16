6d0b7485d367ecae6a724ed972e3e10e54f4f93b
Russell Gordon <rgordon@lcs.on.ca>
2024-12-16 13:04:13 -0500

    Images example

--- ImagesExample/ContentView.swift
+++ ImagesExample/ContentView.swift
@@ -10,10 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
+            // SF Symbol
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+                .border(.green)
+            
+            // From Assets catalog
+            Image("example")
+                .resizable()
+                .scaledToFit()
+                .border(.red)
+
             Text("Hello, world!")
+                .border(.purple)
         }
         .padding()
     }
