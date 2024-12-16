35b2b59046ec7714808a0cae7917e9ae2cd98df1
Russell Gordon <rgordon@lcs.on.ca>
2024-12-16 13:02:12 -0500

    Initial Commit

---
+++ ImagesExample/ImagesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesExampleApp.swift
+//  ImagesExample
+//
+//  Created by Russell Gordon on 2024-12-16.
+//
+
+import SwiftUI
+
+@main
+struct ImagesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
